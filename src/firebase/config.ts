4c7cbd4bd25d12bc193d04d05bd3175615018282
Jennifer Kirkland <jen.kirkland@gmail.com>
2026-03-07 13:58:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9154456813-5c3e4",
+  "appId": "1:893753508111:web:07a13325d689113332fe7a",
+  "apiKey": "AIzaSyA2DRP-hdT_hznp_a94yQtWwxIY5hNrwAA",
+  "authDomain": "studio-9154456813-5c3e4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "893753508111"
+};
